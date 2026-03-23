ac806646aba1df2ca02b3e52c4d8552d74d6ffc9
code-islah <codeislah@gmail.com>
2026-03-24 02:42:12 +0600

    axios base changed

--- client/src/API/axios.js
+++ client/src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bayan-connector-backend.vercel.app/api",
+  baseURL: "https://bayan-connector.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
